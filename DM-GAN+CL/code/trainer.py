63cef9fb8c4c33dd0ac5d0bfd35279ec680d9a20
ts170 <ts170@hdm-stuttgart.de>
2021-09-19 19:20:24 +0000

    new approach: utilize gen_esample function starting with new yaml to generate examples from preprocessed coco

--- DM-GAN+CL/code/trainer.py
+++ DM-GAN+CL/code/trainer.py
@@ -689,44 +689,89 @@ class GANSampler(object):
             im = Image.fromarray(ndarr)
             im.save(fullpath)
 
-    def sampling(self, split_dir):
+    def gen_example(self, data_dic):
         if cfg.TRAIN.NET_G == '':
             print('Error: the path for morels is not found!')
         else:
-            if split_dir == 'test':
-                split_dir = 'valid'
             # Build and load the generator
-            if cfg.GAN.B_DCGAN:
-                netG = G_DCGAN()
-            else:
-                netG = G_NET()
-            netG.apply(weights_init)
-            netG.cuda()
-            netG.eval()
-
-            # load text encoder
-            text_encoder = RNN_ENCODER(self.n_words, nhidden=cfg.TEXT.EMBEDDING_DIM)
-            state_dict = torch.load(cfg.TRAIN.NET_E, map_location=lambda storage, loc: storage)
+            text_encoder = \
+                RNN_ENCODER(self.n_words, nhidden=cfg.TEXT.EMBEDDING_DIM)
+            state_dict = \
+                torch.load(cfg.TRAIN.NET_E, map_location=lambda storage, loc: storage)
             text_encoder.load_state_dict(state_dict)
             print('Load text encoder from:', cfg.TRAIN.NET_E)
             text_encoder = text_encoder.cuda()
             text_encoder.eval()
 
-            batch_size = self.batch_size
-            nz = cfg.GAN.Z_DIM
-            noise = Variable(torch.FloatTensor(batch_size, nz), volatile=True)
-            noise = noise.cuda()
-
+            # the path to save generated images
+            if cfg.GAN.B_DCGAN:
+                netG = G_DCGAN()
+            else:
+                netG = G_NET()
+            s_tmp = cfg.TRAIN.NET_G[:cfg.TRAIN.NET_G.rfind('.pth')]
             model_dir = cfg.TRAIN.NET_G
-            state_dict = torch.load(model_dir, map_location=lambda storage, loc: storage)
-            # state_dict = torch.load(cfg.TRAIN.NET_G)
+            state_dict = \
+                torch.load(model_dir, map_location=lambda storage, loc: storage)
             netG.load_state_dict(state_dict)
             print('Load G from: ', model_dir)
+            netG.cuda()
+            netG.eval()
+            for key in data_dic:
+                save_dir = '%s/%s' % (s_tmp, key)
+                mkdir_p(save_dir)
+                captions, cap_lens, sorted_indices = data_dic[key]
 
-            # the path to save generated images
-            s_tmp = model_dir[:model_dir.rfind('.pth')]
-            save_dir = '%s/%s' % (s_tmp, split_dir)
-            mkdir_p(save_dir)
+                batch_size = captions.shape[0]
+                nz = cfg.GAN.Z_DIM
+                captions = Variable(torch.from_numpy(captions), volatile=True)
+                cap_lens = Variable(torch.from_numpy(cap_lens), volatile=True)
 
-            cnt = 0
-            cont = True
+                captions = captions.cuda()
+                cap_lens = cap_lens.cuda()
+                for i in range(1):  # 16
+                    noise = Variable(torch.FloatTensor(batch_size, nz), volatile=True)
+                    noise = noise.cuda()
+                    #######################################################
+                    # (1) Extract text embeddings
+                    ######################################################
+                    hidden = text_encoder.init_hidden(batch_size)
+                    # words_embs: batch_size x nef x seq_len
+                    # sent_emb: batch_size x nef
+                    words_embs, sent_emb = text_encoder(captions, cap_lens, hidden)
+                    mask = (captions == 0)
+                    #######################################################
+                    # (2) Generate fake images
+                    ######################################################
+                    noise.data.normal_(0, 1)
+                    fake_imgs, attention_maps, _, _ = netG(noise, sent_emb, words_embs, mask, cap_lens)
+                    # G attention
+                    cap_lens_np = cap_lens.cpu().data.numpy()
+                    for j in range(batch_size):
+                        save_name = '%s/%d_s_%d' % (save_dir, i, sorted_indices[j])
+                        for k in range(len(fake_imgs)):
+                            im = fake_imgs[k][j].data.cpu().numpy()
+                            im = (im + 1.0) * 127.5
+                            im = im.astype(np.uint8)
+                            # print('im', im.shape)
+                            im = np.transpose(im, (1, 2, 0))
+                            # print('im', im.shape)
+                            im = Image.fromarray(im)
+                            fullpath = '%s_g%d.png' % (save_name, k)
+                            im.save(fullpath)
+
+                        for k in range(len(attention_maps)):
+                            if len(fake_imgs) > 1:
+                                im = fake_imgs[k + 1].detach().cpu()
+                            else:
+                                im = fake_imgs[0].detach().cpu()
+                            attn_maps = attention_maps[k]
+                            att_sze = attn_maps.size(2)
+                            img_set, sentences = \
+                                build_super_images2(im[j].unsqueeze(0),
+                                                    captions[j].unsqueeze(0),
+                                                    [cap_lens_np[j]], self.ixtoword,
+                                                    [attn_maps[j]], att_sze)
+                            if img_set is not None:
+                                im = Image.fromarray(img_set)
+                                fullpath = '%s_a%d.png' % (save_name, k)
+                                im.save(fullpath)
